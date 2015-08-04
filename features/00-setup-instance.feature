5cae7ea23415fb5d80d76fb0f015a2caff783598
Maxwell Morais <max@san-home.com>
2015-08-04 02:13:49 +0000

    Added features and steps sources

--- features/00-setup-instance.feature
+++ features/00-setup-instance.feature
@@ -1,1 +1,77 @@
-../../san_features/features/00-setup-instance.feature
+Feature: Ensuring that the instance works, and the UI loads fine
+In order to make sure that ERPNext works
+As a User doing your first login
+I open the Instance URL using salad
+
+    Scenario: First Login
+        Given I am logged in as "Administrator" passing "admin"
+        When I look around
+        Then I should see "ERPNext" anywhere in the page
+
+    Scenario: Instance Setup - Step 1 [The Language]
+        Given I look around
+        When I select the option "english" in the field "language" 
+            And I move to next
+        Then I should see "Region" somewhere in the page
+
+    Scenario: Instance Setup - Step 2 [The Location]
+        Given I look around
+        When I select the option "United States" in the field "country"
+            And I select the option "America/New_York" in the field "timezone"
+            And I select the option "USD" in the field "currency"
+            And I move to next
+        Then I should see "The First User: You" somewhere in the page
+
+    Scenario: Instance Setup - Step 3 [System Admin]
+        Given I look around
+        When I fill the field "first_name" with "Test"
+            And I fill the field "last_name" with "User"
+            And I fill the field "email" with "test_demo@our_company.com"
+            And I fill the field "password" with "test"
+            And I move to next
+        Then I should see "The Organization" somewhere in the page
+
+    Scenario: Instance Setup - Step 4 [The Organization]
+        Given I look around
+        When I fill the field "company_name" with "Wind Power LLC"
+            And I fill the field "company_abbr" with "WPL"
+            And I fill the field "company_tagline" with "Wind Mills for a Better Tomorrow"
+            And I fill the field "bank_account" with "Citibank"
+            And I select the option "Standard" in the field "chart_of_accounts"
+            And I fill the field "fy_start_date" with "01-01-2015"
+            And I fill the field "fy_end_date" with "12-31-2015"
+            And I move to next
+        Then I should see "The Brand" somewhere in the page
+
+    Scenario: Instance Setup - Step 5 [The Brand]
+        Given I look around 
+        When I move to next
+        Then I should see "Add Users" somewhere in the page
+
+    Scenario: Instance Setup - Step 6 [The Users]
+        Given I look around
+        When I move to next
+        Then I should see "Add Taxes" somewhere in the page
+
+    Scenario: Instance Setup - Step 7 [The Taxes]
+        Given I look around
+        When I move to next
+        Then I should see "Your Customers" somewhere in the page
+
+    Scenario: Instance Setup - Setup 8 [The Customers]
+        Given I look around
+        When I fill the field "customer_1" with "Standard Customer"
+            And I move to next
+        Then I should see "Your Suppliers" somewhere in the page
+
+    Scenario: Instance Setup - Setup 9 [The Supplier]
+        Given I look around
+        When I fill the field "supplier_1" with "Standard Supplier"
+            And I move to next
+        The I should see "Your Products or Services" somewhere in the page
+
+    Scenario: Instance Setup - Setup 10 [The Products or Services]
+        Given I look around
+        When I fill the field "item_1" with "Standard Product"
+            And I complete the setup
+        Then should see "Test User" somewhere in the page
--- features/steps.py
+++ features/steps.py
@@ -1,1 +1,103 @@
-../../san_features/features/steps.py
+# -*- coding: utf-8 -*-
+from lettuce import step, world, before
+from salad.logger import logger
+
+@step(r"go to the home page")
+def go_to_home(step):
+    step.given('I visit the url "http://localhost:8000"')
+
+@step(r'I am logged in as "([^"]*)" using "([^"])"')
+def is_logged_in(step, user, pwd):
+    step.behave_as(
+        """
+        Given I go to the home page
+            And I click on the link with the text "Login"
+            And I wait 1 second
+            And I click on the field with the id "login_email"
+            And I fill in the field with id "login_email" with "{user}"
+            And I click on the field with the id "login_password"
+            And I fill in the field with id "login_password" with "{pwd}"
+            And I click the button with the css selector ".btn-login"
+            And I wait 2 seconds
+        """.format(user=user, pwd=pwd)
+    )
+
+@step(u"go to the desk")
+def go_to_desk(step):
+    step.given('I click on the element with the css selector ".navbar-home"')
+
+@step('select the option "([^"]*)" in the field "([^"]*)"')
+def select_frappe_field(step, option, selector):
+    step.behave_as("""
+        Given I scroll to "{selector}"
+            And I click on the element with the xpath "{option}"
+            And I hit the ESCAPE key
+    """.format(selector="select[data-fieldname='{}']".format(selector), 
+                option="//option[@value='{}']".format(option)))
+
+@step(u'fill the field "([^"]*)" with "([^"]*)"')
+def fill_frappe_field(step, selector, value):
+    step.behave_as("""
+        Given I scroll to "{selector}" 
+            And I click on the field with the css selector "{selector}"
+            And I fill in the field with the css selector "{selector}" with "{value}"
+    """.format(selector="input[data-fieldname='{}']".format(selector), value=value))
+
+@step(u'check the field "([^"]*)"')
+def check_frappe_field(step, selector):
+    step.given('I click on the field with the css selector "selector"'.format(
+        selector="input[data-fieldname='{}']".format(selector)
+    ))
+
+@step(u'turn (on|off) the field "([^"]*)"')
+def turn_to_frappe(step, turn_to, selector):
+    step.behave_as()
+
+@step('move to ([Nn]ext|[[Pp]rev]ious)')
+def moving(step, move_to):
+    page = str(int(world.browser.url[-1],10)+1)
+    humanized = page + ('st' if page[-1] == '1' else 'nd' if page[-1] == '2' else 'rd' if page[-1] == '3' else 'th')
+    step.given('I click on the {} element with the css selector "a.next-btn.btn.btn-primary.btn-sm"'.format(humanized))
+
+@step('I complete the setup')
+def completing_setup(step):
+    step.behave_as("""Given I click on the last element with the css selector "a.complete-btn.btn.btn-primary.btn-sm"
+        And I wait 30 seconds""")
+
+@step('scroll to "([^"]*)"')
+def scroll_to(step, selector):
+    try:
+        world.browser.execute_script('frappe.ui.scroll("{}");'.format(selector));
+    except NotImplementedError:
+        logger.info("Attemped to run javascript in javascript-disabled browser. Movin along.")
+
+@step('go to the module "([^"]*)"')
+def go_to_module(step, module):
+    
+    step.behave_as("""
+    Given I go to the desk
+        And I click on the element with the css selector "{}"
+        And I wait for ajax
+    """.format(selector))
+
+@step('add a new doc "([^"]*)/([^"]*)"')
+def add_new_doc(step, module, doctype):
+    pass
+
+@step('add a child on "([^"]*)"')
+def add_child(step, table):
+    step.behave_as("""
+        Given I scrool to "{selector}
+            And I click on the element with the css selector "{selector}"
+    """.format(selector="[data-fieldname='{}'] .grid-row-open .btn-success".format(table)))
+
+@step('save the child "([^"]*)"')
+def save_the_child(step, table):
+    selector = '[data-fieldname="{0}"] .grid-row-open .btn-success'.format(table)
+    step.given('I click on the element with the css selector "{}"'.format(selector))
+
+@before.each_scenario
+def ensure_that_setup_scenario_run_in_setup_wizard(scenario):
+    if 'Setup' in scenario.name and not '#setup-wizard' in world.browser.url:
+        scenario.steps = []
+        logger.info("Attemped to do the setup in a instance with the setup already done. Movin along.")
