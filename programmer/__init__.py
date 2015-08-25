ea1f05b400d08b06711da3bd3ac63ea947103894
MaxMorais <max.morais.dmm@gmail.com>
2015-08-25 04:21:43 +0000

    oAuth Process Refactor

--- programmer/__init__.py
+++ programmer/__init__.py
@@ -2,6 +2,6 @@
 import frappe
 
 @frappe.whitelist()
-def store_oauth_token(code, state, **kw):
+def authorized(code=None, state=None, **kw):
     from programmer.doctype.app_token.app_token import _store_oauth_token
     return _store_oauth_token(code, state)
--- programmer/programmer/doctype/app_token/app_token.py
+++ programmer/programmer/doctype/app_token/app_token.py
@@ -5,30 +5,22 @@
 from __future__ import unicode_literals
 import frappe
 import json
+import time
 
+from collections import defaultdict
 from rauth import OAuth2Service
 from frappe.model.document import Document
 
-class APPTokenError(Exception):
-	pass
-
 class APPToken(Document):
 	def on_update(self):
-		auth = filter(lambda x: x.user==self.owner, self.authenticated_users)
-		if not auth or not auth[0].auth_token:
-			request_auth_code(self.name, self.owner)
-		try:
-			get_oauth_session(self.name, self.owner)
-		except KeyError, e:
-			frappe.msgprint(e)
-			frappe.db.commit()
-			request_oauth_code(self.name, self.owner)
+		pass
 
-def traverse_params(param_list, replacements={}):
+def get_traversed_params(param_list, replacements={}):
+	""" Adds the replacements in the param list """
 	response = {}
 	for row in param_list:
 		value = row.value
-		if value.startswith(':'):
+		if value and value.startswith(':'):
 			value = value.replace(':', '')
 			if hasattr(replacements, value):
 				value = getattr(replacements, value)
@@ -36,146 +28,137 @@ def traverse_params(param_list, replacements={}):
 				value = replacements[value]
 			elif hasattr(replacements, 'get'):
 				value = replacements.get(value)
-		response[row.key] = value
+			response[row.key] = value
 	return response
 
-def get_oauth_service(doc):
-	return OAuth2Service(
-		name=doc.title,
-		client_id=doc.client_id,
-		client_secret=doc.client_secret,
-		authorize_url=doc.authorize_url,
-		access_token_url=doc.access_token_url,
-		base_url=doc.base_url
+def get_oauth_service(service):
+	""" Returns the OAuth2Service instance """
+	fields = frappe.db.get_values(
+		"APP Token",
+		service,
+		["name", "client_id", "cliend_secret", "autorization_url", "access_token_url", "base_url"]
 	)
+	return OAuth2Service(**fields)
+
+def get_traversed_data(service, field, repl={}):
+	""" Format the request headers """
+	dl = frappe.get_all("APP Token Params", fields=["key", "value"], filters=[
+		["APP Token Params", "parent", "=", service],
+		["APP Token Params", "params", "=", field]
+	])
+	return get_traversed_params(dl, repl)
+
+def get_oauth_headers_data(service, repl={}):
+	return get_traversed_data(service, "session_headers_params", repl)
+
+def get_oauth_request_data(service, repl={}):
+	return get_traversed_data(service, "params", repl)
+
+def get_oauth_session_data(service, repl={}):
+	return get_traversed_data(service, "session_data_params", repl)
+
+def get_user(user=None):
+	return user or frappe.get_user().name
+	
+def get_oauth_user(service, user):
+	""" Return the user id in the Service """
+	dl = frappe.get_all("APP Token User", fields="app_user_id", filters=[
+		["APP Token User", "user", "=", user],
+		["APP Token User", "parent", "=", service]
+	])
+	return dl[0].app_user_id if dl else None
 
 @frappe.whitelist()
 def request_oauth_code(service, user=None):
-	doc = frappe.get_doc('APP Token', service)
-	if not user:
-		user = frappe.get_user().name
-	params = traverse_params(doc.params, replacements=doc)
 	state = frappe.db.set_temp(json.dumps({
-		'service': service, 
-		'user': user, 
-		'parser': doc.user_id_parser,
-		'endpoint': doc.api_user_endpoint }))
-	params['state'] = state
-	oserv = get_oauth_service(doc)
-	url = oserv.get_authorize_url(**params)
+		"service": service,
+		"user": user
+	}))
+
+	data = get_oauth_request_data(service, {'state': state})
+	flow = get_oauth_service(service)
+	url = flow.get_authorize_url(**data)
 
 	msg = '''
-		Visit <a class="btn btn-xs btn-default" href="{url}" target="_blank">this link</a>  for autorize <b>{name}</b> in this app.
+	Visit <a class="btn btn-xs btn-default" href="{url}" target="_blank">this link</a> for autorize <b>{name}</b> in this app.
 	'''.format(url=url, name=doc.name)
 
 	frappe.msgprint(msg)
 
 def _store_oauth_token(code=None, state=None):
 	if not code:
-		return {'FAIL': 'You did not authorized the request'}
-	from programmer.data_tools import get_uri_parsed
+		return {"FAIL": frappe._("You did not authorized the request")}
 
-	data = json.loads(frappe.db.get_temp(state))
-	service = data['service']
-	user = data['user']
-	filters = [
-		["APP Token User", "parent", "=", service],
-		["APP Token User", "user", "=", user]
-	]
-	doc = frappe.get_doc("APP Token", service)
+	if state:
+		try:
+			raw = json.loads(frappe.db.get_temp(state))
+			user = raw["user"]
+			service = raw["service"]
+		except ValueError:
+			raise {"FAIL": frappe._("The given status dont match with the status provided")}
 
-	replacers = {
-		"auth_token": code
-	}
+	headers = get_oauth_headers_data(service, {"code": code, "state": state})
+	data = get_oauth_session_data(service, {"code": code, "state": state})
+	flow = get_oauth_service(service)
 
-	oserv = get_oauth_service(doc)
-	data = traverse_params(doc.session_data_params, replacements=replacers)
-	
-	response = oserv.get_raw_access_token(data=data).json()
-
-	dl = frappe.get_all("APP Token User", fields=["name", "app_user_id"], filters=filters)
-
-	if dl:
-		user_token_name = dl[0].name
-		d = frappe.get_doc("APP Token User", user_token_name)
-		d.update({
-			"auth_token": response["access_token"],
-			"expires_in": response["expires_in"],
-			"refresh_token": response["refresh_token"]
-		})
-		d.save()
+	src = frappe.db.get_values("APP Token", service, ["session_given_by", "api_user_endpoint"])
+	if src.session_given_by == "Simple Session":
+		session = flow.get_auth_session(data=data)
+		response = session.get(src.api_user_endpoint).json()
+		update_token(
+			service, 
+			response["auth_token"], 
+			user, 
+			response["expires_in"], 
+			response["refresh_token"]
+		)
 	else:
-		d = frappe.new_doc("APP Token User")
-		d.update({
-			"parent": service,
-			"parenttype": "APP Token",
-			"parentfield": "authenticated_users",
-			"user": user,
-			"auth_token": response["access_token"],
-			"expires_in": response["expires_in"],
-			"refresh_token": response["refresh_token"]
-		})
-		d.save()
-		user_token_name = d.name
+		session = flow.get_raw_access_token(data=data)
+		response = session.get(src.api_user_endpoint)
+		update_token(service, 
+			response.auth_token,
+			user
+		)
+	return {'OK': frappe._("Token stored successfull")}
 
+def update_token(service, token="", user=None, expires_in=-1, refresh=None):
+	user = get_user(user)
+	dl = frappe.get_all("APP Token User", fields=["name", "auth_token"], filters=[
+		["APP Token User", "user", "=", user],
+		["APP Token User", "parent", "=", service]
+	])
+	if dl:
+		doc = frappe.get_doc("APP Token User", dl["name"])
+	else:
+		doc = frappe.new_doc("APP Token User")
+	data = {
+		"auth_token": token,
+		"expires_in": expires_in,
+		"refresh_token": refresh
+	}
+	doc.update(data)
+	doc.save()
 	frappe.db.commit()
 
-	if dl and (not dl[0].app_user_id) and data.has_key("parser") and data.has_key("endpoint") and all([data["parser"], data["endpoint"]]):
-		app_user_id = get_uri_parsed(service, data["endpoint"], data["parser"], user=user, code=code)
-	else:
-		app_user_id = None
+	return token
 
-		if app_user_id and (not dl[0].app_user_id):
-			frappe.db.set_value("APP Token User", user_token_name, "app_user_id", app_user_id)
-			frappe.db.commit()
-
-	return {'OK': frappe._('Authorization Code Stored'), 'autorized': response}
+def get_token(service, user=None):
+	user = get_user(user)
+	dl = frappe.get_all("APP Token User", fields="auth_token", filters=[
+		["APP Token User", "user", "=", user],
+		["APP Token User", "parent", "=", service]
+	])
+	if dl:
+		return dl[0].auth_token
 
 def clear_oauth_token(service, user=None):
-	if not user:
-		user = frape.get_user().name
-
-	filters = [
-		["APP Token User", "parent", "=", service],
-		["APP Token User", "user", "=", user]
-	]
-
-	dl = frappe.get_all("APP Token User", fields=["name", "auth_token"], filters=filters)
-	if not dl or dl[0].auth_token:
-		return
-	frappe.db.set_value("APP Token User", dl[0].name, "auth_token", "")
-	frappe.db.commit()
+	return get_update_token(service, "", user)
 
 def refresh_oauth_token(service, user=None):
-	if not user:
-		user = frape.get_user().name
-
-	filters = [
-		["APP Token User", "parent", "=", service],
-		["APP Token User", "user", "=", user]
-	]
-	dl = frappe.get_all("APP Token User", fields=["name"], filters=filters)
-	if dl and dl[0].name:
-		d = frappe.get_doc("APP Token User", dl[0]["name"])
-	doc = frappe.get_doc("APP Token", service)
-	data = {
-		'refresh_token': d.refresh_token,
-		'grant_type': 'refresh_token'
-	}
-	osrv = get_oauth_service(doc)
-	response = osrv.get_raw_access_token(data=data).json()
-	d.update({
-		"auth_token": response["access_token"],
-		"expires_in": response["expires_in"],
-		"refresh_token": response["refresh_token"]	
-	})
-	d.save()
-	frappe.db.commit()
-	return response["access_token"]
+	pass
 
 def get_oauth_session(service, user=None):
-	from time import time
-
+	
 	current = int(time())
 
 	if not user:
@@ -190,7 +173,7 @@ def get_oauth_session(service, user=None):
 
 	if not dl or not dl[0].auth_token:
 		return request_oauth_code(service, user)
-	elif dl[0].expires_in and (dl[0].expires_in - current) <= 0:
+	elif dl[0].expires_in and dl.expires_in > -1 and (dl[0].expires_in - current) <= 0:
 		auth_token = refresh_oauth_token(service, user)
 	else:
 		auth_token = dl[0].auth_token
@@ -198,6 +181,14 @@ def get_oauth_session(service, user=None):
 	doc = frappe.get_doc("APP Token", service)	
 	oserv = get_oauth_service(doc)
 	session = oserv.get_session(auth_token)
+	response = session.get(doc.api_user_endpoint)
 
+	if response.status_code != 200:
+		replacers = {
+			"auth_token": auth_token
+		}
+		data = traverse_params(doc.session_data_params, replacements=replacers)		
+		session = oserv.get_auth_session(data=data)
+		
 	return session
 	
